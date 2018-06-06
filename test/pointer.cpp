3f408c20351ad881b8a7082eefcac0215c03b0a4
calmwu <wubo0067@hotmail.com>
2018-06-06 10:59:22 +0800

    c++17的代码

---
+++ test/pointer.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include <vector>
+#include <iostream>
+#include <memory>
+#include <algorithm>
+
+void test_uniqueptr() {
+	std::cout << "test_uniqueptr" << std::endl;
+
+	std::vector<std::unique_ptr<int> > numVector;
+	
+	std::unique_ptr<int> up1 = std::make_unique<int>(1);
+	numVector.push_back(std::move(up1));
+
+	numVector.push_back(std::make_unique<int>(2));
+
+	auto printNum = [](const std::unique_ptr<int> &up) {
+		std::cout << *up << std::endl;
+	};
+
+	std::for_each(numVector.begin(), numVector.end(), printNum);
+	return;
+}
