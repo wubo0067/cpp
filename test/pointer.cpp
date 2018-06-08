4613f3de18a293c3ba34b5c788e56f2082e6917f
calmwu <wubo0067@hotmail.com>
2018-06-08 12:46:22 +0800

    更新clangformat文件

--- test/pointer.cpp
+++ test/pointer.cpp
@@ -1,23 +1,24 @@
-#include "stdafx.h"
-#include <vector>
+#include <algorithm>
 #include <iostream>
 #include <memory>
-#include <algorithm>
+#include <vector>
+#include "stdafx.h"
 
-void test_uniqueptr() {
-	std::cout << "test_uniqueptr" << std::endl;
+void test_uniqueptr()
+{
+    std::cout << "test_uniqueptr" << std::endl;
 
-	std::vector<std::unique_ptr<int> > numVector;
-	
-	std::unique_ptr<int> up1 = std::make_unique<int>(1);
-	numVector.push_back(std::move(up1));
+    std::vector< std::unique_ptr< int > > numVector;
 
-	numVector.push_back(std::make_unique<int>(2));
+    std::unique_ptr< int > up1 = std::make_unique< int >( 1 );
+    numVector.push_back( std::move( up1 ) );
 
-	auto printNum = [](const std::unique_ptr<int> &up) {
-		std::cout << *up << std::endl;
-	};
+    numVector.push_back( std::make_unique< int >( 2 ) );
 
-	std::for_each(numVector.begin(), numVector.end(), printNum);
-	return;
+    auto printNum = []( const std::unique_ptr< int >& up ) {
+        std::cout << *up << std::endl;
+    };
+
+    std::for_each( numVector.begin(), numVector.end(), printNum );
+    return;
 }
--- test/printtype.cpp
+++ test/printtype.cpp
@@ -3,32 +3,29 @@
 #include <cstring>
 #include <ostream>
 
-inline
-std::ostream&
-operator<<(std::ostream& os, string_view const& s)
+inline std::ostream&
+operator<<( std::ostream& os, string_view const& s )
 {
-    return os.write(s.data(), s.size());
+    return os.write( s.data(), s.size() );
 }
 
-template <class T>
-constexpr
-std::string_view
-type_name()
+template < class T >
+constexpr std::string_view type_name()
 {
     using namespace std;
 #ifdef __clang__
     string_view p = __PRETTY_FUNCTION__;
-    return string_view(p.data() + 34, p.size() - 34 - 1);
-#elif defined(__GNUC__)
+    return string_view( p.data() + 34, p.size() - 34 - 1 );
+#elif defined( __GNUC__ )
     string_view p = __PRETTY_FUNCTION__;
-#  if __cplusplus < 201402
-    return string_view(p.data() + 36, p.size() - 36 - 1);
-#  else
-    return string_view(p.data() + 49, p.find(';', 49) - 49);
-#  endif
-#elif defined(_MSC_VER)
+#if __cplusplus < 201402
+    return string_view( p.data() + 36, p.size() - 36 - 1 );
+#else
+    return string_view( p.data() + 49, p.find( ';', 49 ) - 49 );
+#endif
+#elif defined( _MSC_VER )
     string_view p = __FUNCSIG__;
-    return string_view(p.data() + 84, p.size() - 84 - 7);
+    return string_view( p.data() + 84, p.size() - 84 - 7 );
 #endif
 }
 
@@ -36,20 +33,19 @@ int& foo_lref();
 int&& foo_rref();
 int foo_value();
 
-int
-main()
+int main()
 {
     int i = 0;
     const int ci = 0;
-    std::cout << "decltype(i) is " << type_name<decltype(i)>() << '\n';
-    std::cout << "decltype((i)) is " << type_name<decltype((i))>() << '\n';
-    std::cout << "decltype(ci) is " << type_name<decltype(ci)>() << '\n';
-    std::cout << "decltype((ci)) is " << type_name<decltype((ci))>() << '\n';
-    std::cout << "decltype(static_cast<int&>(i)) is " << type_name<decltype(static_cast<int&>(i))>() << '\n';
-    std::cout << "decltype(static_cast<int&&>(i)) is " << type_name<decltype(static_cast<int&&>(i))>() << '\n';
-    std::cout << "decltype(static_cast<int>(i)) is " << type_name<decltype(static_cast<int>(i))>() << '\n';
-    std::cout << "decltype(foo_lref()) is " << type_name<decltype(foo_lref())>() << '\n';
-    std::cout << "decltype(foo_rref()) is " << type_name<decltype(foo_rref())>() << '\n';
-    std::cout << "decltype(foo_value()) is " << type_name<decltype(foo_value())>() << '\n';
-	return 0;
+    std::cout << "decltype(i) is " << type_name< decltype( i ) >() << '\n';
+    std::cout << "decltype((i)) is " << type_name< decltype( ( i ) ) >() << '\n';
+    std::cout << "decltype(ci) is " << type_name< decltype( ci ) >() << '\n';
+    std::cout << "decltype((ci)) is " << type_name< decltype( ( ci ) ) >() << '\n';
+    std::cout << "decltype(static_cast<int&>(i)) is " << type_name< decltype( static_cast< int& >( i ) ) >() << '\n';
+    std::cout << "decltype(static_cast<int&&>(i)) is " << type_name< decltype( static_cast< int&& >( i ) ) >() << '\n';
+    std::cout << "decltype(static_cast<int>(i)) is " << type_name< decltype( static_cast< int >( i ) ) >() << '\n';
+    std::cout << "decltype(foo_lref()) is " << type_name< decltype( foo_lref() ) >() << '\n';
+    std::cout << "decltype(foo_rref()) is " << type_name< decltype( foo_rref() ) >() << '\n';
+    std::cout << "decltype(foo_value()) is " << type_name< decltype( foo_value() ) >() << '\n';
+    return 0;
 }
