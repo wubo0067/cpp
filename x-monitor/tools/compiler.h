6f3abc0fe61112b1d8df7feb2455e08cb9f00b67
calmwu <wubo0067@hotmail.com>
2021-10-17 21:18:25 +0800

    always_inline

--- x-monitor/tools/compiler.h
+++ x-monitor/tools/compiler.h
@@ -16,3 +16,13 @@
 #define BUILD_BUG_ON_ZERO( e ) ( sizeof( struct { int : -!!( e ); } ) )
 #define __must_be_array( a ) BUILD_BUG_ON_ZERO( __same_type( ( a ), &( a )[0] ) )
 #define ARRAY_SIZE( arr ) ( sizeof( arr ) / sizeof( ( arr )[0] ) + __must_be_array( arr ) )
+
+// 定义纯函数
+#ifndef __pure
+#define  __pure		__attribute__((pure))
+#endif
+
+// 强制内嵌函数
+#ifndef __always_inline
+# define __always_inline	inline __attribute__((always_inline))
+#endif
